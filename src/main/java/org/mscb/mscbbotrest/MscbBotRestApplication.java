d49e9da3f036a2ad30553cb73f0e7339087d07d3
Samuu <samuelfilipevianadois@gmail.com>
2025-08-04 15:55:28 -0300

    feat: first commit

---
+++ src/main/java/org/mscb/mscbbotrest/MscbBotRestApplication.java
@@ -0,0 +1,13 @@
+package org.mscb.mscbbotrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscbBotRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MscbBotRestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/mscb/mscbbotrest/MscbBotRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mscb.mscbbotrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscbBotRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
